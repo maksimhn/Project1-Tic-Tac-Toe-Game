ec7e26fcc531831f9c1386ed44657e74b8845dfe
Maksim <dollydasheep@gmail.com>
2015-07-08 14:16:51 -0400

    Revamp design and inner mechanics
    Revamps design approach to the interface (buttons and fields) and game inner mechanics (game logic)

--- app/app.js
+++ app/app.js
@@ -1,95 +1,73 @@
-var cleanBoard = {
-  a: null,
-  b: null,
-  c: null,
-  d: null,
-  e: null,
-  f: null,
-  g: null,
-  h: null,
-  i: null
-};
 
-var currentBoard = {
-  a: null,
-  b: null,
-  c: null,
-  d: null,
-  e: null,
-  f: null,
-  g: null,
-  h: null,
-  i: null
-};
-
+var player, move, turnCount, playerToken;
+var currentTurn = '';
+var cleanBoard = ["","","","","","","","",""];
+var currentBoard = ["","","","","","","","",""];
 
 var getWinner = function () {
+  console.log(currentBoard);
   if (winnerIs('x')) {
     return 'x';
-  }
-  if (winnerIs('o')) {
+  } else if (winnerIs('o')) {
     return 'o';
+  }
+  for (var i = 0; i < currentBoard.length; i++) {
+    if (currentBoard[i] !== '') {
+      return 'draw'
+    }
   }
   return null;
 };
 
+// updates the local version of the board with the one sent by server
+var boardRender = function (board) {
+  for (var i = 0; i < board.length; i++) {
+    $('#cell' + i).text(board[i]);
+  }
+};
+
+var game = function() {
+
+};
+
+// block of functions to determine if there is a winner
 var winnerIs = function (player) {
   return winsRow(player) || winsColumn(player) || winsDiagonal(player);
 };
-
 var winsRow = function (player) {
-  return allThree(player, currentBoard.a, currentBoard.b, currentBoard.c) ||
-         allThree(player, currentBoard.d, currentBoard.e, currentBoard.f) ||
-         allThree(player, currentBoard.g, currentBoard.h, currentBoard.i);
+  return allThree(player, currentBoard[0], currentBoard[1], currentBoard[2]) ||
+         allThree(player, currentBoard[3], currentBoard[4], currentBoard[5]) ||
+         allThree(player, currentBoard[6], currentBoard[7], currentBoard[8]);
 };
-
 var winsColumn = function (player) {
-  return allThree(player, currentBoard.a, currentBoard.d, currentBoard.g) ||
-         allThree(player, currentBoard.b, currentBoard.e, currentBoard.h) ||
-         allThree(player, currentBoard.c, currentBoard.f, currentBoard.i);
+  return allThree(player, currentBoard[0], currentBoard[3], currentBoard[6]) ||
+         allThree(player, currentBoard[1], currentBoard[4], currentBoard[7]) ||
+         allThree(player, currentBoard[2], currentBoard[5], currentBoard[8]);
 };
-
 var winsDiagonal = function (player) {
-  return allThree(player, currentBoard.a, currentBoard.e, currentBoard.i) ||
-         allThree(player, currentBoard.c, currentBoard.e, currentBoard.g);
+  return allThree(player, currentBoard[0], currentBoard[4], currentBoard[8]) ||
+         allThree(player, currentBoard[2], currentBoard[4], currentBoard[6]);
 };
-
 var allThree = function (player, cellOne, cellTwo, cellThree) {
   return (cellOne === player) && (cellTwo === player) && (cellThree === player);
 };
 
-var isItTie = function () {
-  for (cell in currentBoard) {
-    if (currentBoard.cell !== null) {
 
-    }
-  }
-};
 
-var nextMove = function (player, move) {
-  currentBoard[move] = player;
-  if (getWinner(player) === 'x') {
-    return console.log('x has won');
-  } else if (getWinner(player) === 'o') {
-    return console.log('o has won');
-  }
-  return null;
-};
 
-var nextTurn = function () {
 
-};
 
-$(function() {
-  $('#start').click(function() {
-    $('.activeframe').toggleClass('activeframe-down');
-    $('.playersnames').toggleClass('playersnames-shown');
-  });
-});
+// $(function() {
+//   $('#start').click(function() {
+//     $('.activeframe').toggleClass('activeframe-down');
+//     $('.playersnames').toggleClass('playersnames-shown');
+//   });
+// });
 
-$(function() {
-  $('#play').click(function() {
-    $('.activeframe').toggleClass('activeframe-down');
-    $('.playersnames').toggleClass('playersnames-shown');
-  });
-});
+// $(function() {
+//   $('#play').click(function() {
+//     $('.activeframe').toggleClass('activeframe-down');
+//     $('.playersnames').toggleClass('playersnames-shown');
+//   });
+// });
+
